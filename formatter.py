0f9a825b808049ef423c44069c557b84a650dbca
Brian Hoang <bri.hoang@utexas.edu>
2015-09-26 20:45:50 -0500

    sum already calculated, centering in tables

--- formatter.py
+++ formatter.py
@@ -6,8 +6,9 @@ def make_table(mat):
 
     for x in xrange(len(mat[0]) - 1):
         ret += '-:|:'
-    ret += '-:|\n|'
+    ret += '-:|\n'
     for line in mat[1:]:
+        ret += '|'
         for e in line:
             ret += str(e) + '|'
         ret += '\n'
@@ -15,16 +16,14 @@ def make_table(mat):
 
 def make_line_score(team1Name, team1Scores, team2Name, team2Scores):
     header = ['',1,2,3,4]
-    for x in xrange(len(team1Scores) - 4):
+    for x in xrange(len(team1Scores) - 5):
         app = 'OT'
         if x > 0:
             app += str(x+ 1)
         header.append(app)
     header.append('Total')
     team1 = [team1Name] + team1Scores 
-    team1.append(sum(team1Scores))
     team2 = [team2Name] + team2Scores 
-    team2.append(sum(team2Scores))
 
     line = [header, team1, team2]
     return make_table(line)
